f9ba8e9ac923bfbc91a31d54ba36a2aea29a634e
LAPTOP-GH564RCH\Bleu <yonson.dev@gmail.com>
2023-09-08 01:45:43 +0900

    feat: add entry point files for both client and server - The files are pushed along with CMake files.

---
+++ project_chatbleu/ChatBleu_Client/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "ChatBleu Client!" << std::endl;
+
+	return 0;
+}
---
+++ project_chatbleu/ChatBleu_Server/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "ChatBleu Server!" << std::endl;
+
+	return 0;
+}
